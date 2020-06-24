8ceca433a0273b4c015bdb64f3042384b302d667
arirasmana <ari.rasmana@ui.ac.id>
2020-06-24 22:30:03 +0700

    secondpush

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,13 @@ import youtube from '../apis/youtube';
 
 class App extends React.Component {
     onTermSubmit=term=>{
-        console.log(term);
+        youtube.get('/search',
+            {
+                params: {
+                    q: term
+                }
+            });
+       
     };
 
     render() {
